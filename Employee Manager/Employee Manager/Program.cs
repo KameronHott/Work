05f60a6751bd42502c3c3bb3ac393f9cd4d615c8
KameronHott <kameronhott@msn.com>
2014-07-15 07:26:00 -0700

    EmployeeMan

---
+++ Employee Manager/Employee Manager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Employee_Manager.Classes;
+
+namespace Employee_Manager
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Exchange myEx = new Exchange();
+            //myEx.TestEnable();
+            //myEx.enableMailbox("sccdc8pr01.ncu.local/NCU Users/SCC Users/Test Kameron");
+            //myEx.disableMailbox("tkameron");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
